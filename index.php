77b07b6abe69a4b06e2305cd228cb24266517040
Hannah Masila <hannahmasila@gmail.com>
2017-12-04 04:17:00 +0300

    🌚

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php require_once "config.php";?>
 <?php require_once "common.php";?>
 <?php require "coinigy_api.php";?>
+<?php require "poloniex_api.php";?>
+<?php require_once "kraken_api.php";?>
 <?php include "public/templates/header.php"; ?>
 <?php include "public/accounts.php"; ?>
 <?php require "public/withdraw.php"; ?>
-
+<?php require "public/pairs.php";?>
 <?php include "public/templates/footer.php"; ?>
